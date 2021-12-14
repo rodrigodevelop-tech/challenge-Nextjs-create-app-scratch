a2d92157f9edc75f416c8b05c0d3a397ea64e6ca
rodrigodevelop-tech <digoalves12@hotmail.com>
2021-12-14 00:21:45 -0300

    Desenvolvido tela principal com lista de posts e paginacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return(
+    <header className={styles.Headercontainer}>
+      <div className={styles.Headercontent}>
+        <img src="/images/Logo.svg" alt="Logo" />
+      </div>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
 import { getPrismicClient } from '../services/prismic';
+import Link from 'next/dist/client/link';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Prismic from '@prismicio/client';
+
+import { FiCalendar,FiUser } from "react-icons/fi";
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+
 
 interface Post {
   uid?: string;
@@ -24,13 +33,111 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination  } : HomeProps ) {
+  const { next_page } = postsPagination;
+  const [ posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [ nextPage, setNextPage] = useState(next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  async function handleNextPagePosts() : Promise<void>{
 
-//   // TODO
-// };
+    const response = await fetch(next_page);
+
+    const resultsNextPage = await response.json()
+
+    setPosts([
+      ...posts,
+      ...resultsNextPage.results.map((post : Post) => {
+        return {
+          uid:post.uid ,
+          first_publication_date: format(new Date(post.first_publication_date), "d MMM uuuu",{  locale: ptBR  }),
+          data: {
+            title: post.data.title,
+            subtitle: post.data.subtitle,
+            author: post.data.author,
+          }
+        }
+      })
+    ]);
+
+    setNextPage(resultsNextPage.next_page);
+
+  }
+
+
+  return(
+    <>
+      <Head>
+        <title>Home | Posts</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          {
+            posts.map(post => (
+              <Link href={`/posts/${post.uid}`} key={post.uid}>
+                <a >
+                  <h2>{post.data.title}</h2>
+                  <h4> {post.data.subtitle}</h4>
+
+                  <div className={styles.postsFooter}>
+                    <time><FiCalendar/> {post.first_publication_date}</time>
+                    <p><FiUser/> {post.data.author}</p>
+                  </div>
+
+                </a>
+              </Link>
+            ))
+          }
+          {
+            nextPage && (
+              <button
+                type="button"
+                onClick={handleNextPagePosts}
+              >
+                <strong>Carregar mais posts</strong>
+              </button>
+            )
+          }
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps:  GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [
+      Prismic.predicates.at('document.type','pos')
+    ],
+    {
+      pageSize: 4,
+    }
+  );
+
+  const results = postsResponse.results.map( post => {
+
+    return {
+      uid:post.uid ,
+      first_publication_date: format(new Date(post.first_publication_date), "d MMM uuuu",{  locale: ptBR  }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      }
+    }
+  });
+
+  const next_page = postsResponse.next_page;
+
+  return {
+    props: {
+      postsPagination :{
+        results,
+        next_page
+      }
+    }
+  }
+
+};
